036d0ae6dc1b249316172afefd3dd77ca4366ffb
Victor Frunza <victor.frunza96@gmail.com>
2021-09-17 00:14:32 -0700

    base cases for isPrime

--- GitAssignment/GitAssignment/Program.cs
+++ GitAssignment/GitAssignment/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Biggest prime factor");
+
+long n = 600851475143;
+
+
+static bool isPrime(long n)
+{
+    if (n <= 3)
+    {
+        return n >= 2;
+    }
+    if (n % 2 == 0 || n % 3 == 0)
+    {
+        return false;
+    }
+    
+    return true;
+}
